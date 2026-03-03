1260b672061d505988fae43566d466e336ada25b
August Högbom <august.hogbom@gmail.com>
2026-03-03 15:06:30 +0000

    Initial commit

---
+++ Daggerheart-Helper.Web/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+using Daggerheart_Helper.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddAdditionalAssemblies(typeof(Daggerheart_Helper.Shared._Imports).Assembly);
+
+app.Run();
---
+++ Daggerheart-Helper/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Daggerheart_Helper;
+
+public partial class App
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "Daggerheart-Helper" };
+    }
+}
