0cac45ff71254d85e18158ff794b02619b0e7d71
August Högbom <august.hogbom@gmail.com>
2026-04-16 09:47:46 +0200

    DHH-29 Add Domain enum, TraitScores value object, and update GameClass properties

--- Core/Entities/GameClass.cs
+++ Core/Entities/GameClass.cs
@@ -10,8 +10,9 @@ public class GameClass
     public string Description { get; set; } = String.Empty;
     public int BaseEvasion { get; set; }
     public int BaseHealth { get; set; }
-    public Domain Domains { get; set; }
+    public List<Domain>? Domains { get; set; }
+    public TraitScores SuggestedTraits { get; set; } = new(0,0,0,0,0,0);
     public DamageThresholds StartingThresholds { get; set; } = null!;
     public List<Subclass> Subclasses { get; set; } = new();
-    public List<ClassFeature> Features { get; set; } = new();
+    public List<Feature> Features { get; set; } = new();
 }
