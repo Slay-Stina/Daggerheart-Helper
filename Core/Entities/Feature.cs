9d89d759a626d944edaef8a2f5bd14bb8181b4ac
August Högbom <august.hogbom@gmail.com>
2026-04-16 09:44:47 +0200

    DHH-32 Add Armor and TraitChanges entities, update Feature and TraitType definitions

--- Core/Entities/Feature.cs
+++ Core/Entities/Feature.cs
@@ -5,6 +5,5 @@ public class Feature
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public bool IsHopeFeature { get; set; }
-    public bool IsWeaponFeature { get; set; }
+    public List<TraitChanges>? TraitChanges { get; set; }
 }
