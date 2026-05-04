440b7311fd582f343d11252199ef566703583727
August Högbom <august.hogbom@gmail.com>
2026-05-04 15:24:44 +0200

    Daggerheart-Helper-91 Core domain refactor for SRD-aligned model boundaries

--- Core/Entities/Subclass.cs
+++ Core/Entities/Subclass.cs
@@ -8,8 +8,10 @@ public class Subclass
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public TraitType? SpellCastingTraitType { get; set; }
-    
+    public Feature Foundation { get; set; } = null!;
+    public Feature Specialization { get; set; } = null!;
+    public Feature Mastery { get; set; } = null!;
+
     public Guid GameClassId { get; set; }
     public GameClass GameClass { get; set; } = null!;
-    public List<Feature> Features { get; set; } = new();
 }
