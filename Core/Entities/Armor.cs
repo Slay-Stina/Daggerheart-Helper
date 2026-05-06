830b30c3ddff3d1908189b9e9ffe84770e1da819
August Högbom <august.hogbom@gmail.com>
2026-05-06 21:05:52 +0200

    Tests and refactors

--- Core/Entities/Armor.cs
+++ Core/Entities/Armor.cs
@@ -6,9 +6,9 @@ namespace Core.Entities;
 public class Armor
 {
     public int Id { get; set; }
-    public Tier Tier { get; set; }
+    public int Tier { get; set; }
     public string Name { get; set; } = string.Empty;
     public DamageThresholds DamageThresholds { get; set; } = new(0,0);
     public int ArmorScore { get; set; }
-    public List<Feature> Features { get; set; } = new();
+    public Feature? Feature { get; set; }
 }
